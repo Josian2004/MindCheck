25a59fd549a87a6a340c699338119ef668ad95a1
Josian van Efferen <github@josian.nl>
2023-03-22 09:10:02 +0100

    Initial Commit

---
+++ MindCheck/MindCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindCheckApp.swift
+//  MindCheck
+//
+//  Created by Josian van Efferen on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MindCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
