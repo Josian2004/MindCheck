f84cf33fa6fe9f6855d25ef3d01adc4d1e835956
Josian van Efferen <github@josian.nl>
2023-03-26 00:12:56 +0100

    Init

---
+++ MindCheckWatch Watch App/MindCheckWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindCheckWatchApp.swift
+//  MindCheckWatch Watch App
+//
+//  Created by Josian van Efferen on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MindCheckWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
